b20c497cdc9efa8022e53a0a4159dd46af2961eb
Arka Prava Bandyopadhyay <arkahome@gmail.com>
2022-01-09 00:07:56 +0530

    base situation

--- app/main.py
+++ app/main.py
@@ -9,21 +9,3 @@ DB_NAME = "PIAD"
 
 mydb = client["PIAD"]
 
-@app.post('/piad_model_2')
-def fetch_piad_model_2(data: Input_piad_model_2):
-    try:
-        response = mydb['piad_model_2'].find(dict(data))[0]
-        del response['_id']
-        return response
-    except IndexError:
-        return {"error" : "No data."}
-
-
-@app.post('/piad_model_1')
-def fetch_piad_model_1(data: Input_piad_model_1):
-    try:
-        response = mydb['piad_model_1'].find(dict(data))[0]
-        del response['_id']
-        return response
-    except IndexError:
-        return {"error" : "No data."}
--- app/models.py
+++ app/models.py
@@ -2,10 +2,3 @@ from pydantic import BaseModel
 
 
 
-class Input_piad_model_2(BaseModel):
-	EMPLOYEE_ID : str
-
-class Input_piad_model_1(BaseModel):
-	EMPLOYEE_ID : str
-	FIRST_NAME : str
-
