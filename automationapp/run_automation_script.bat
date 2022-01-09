4aad3aed2ac0490e321942951c7620ee1be39f09
Arka Prava Bandyopadhyay <arkahome@gmail.com>
2022-01-09 14:42:08 +0530

    added the database name in argument.

--- automationapp/run_automation_script.bat
+++ automationapp/run_automation_script.bat
@@ -1,2 +1,2 @@
-python automation.py --model_name piad_model_1 --key_cols EMPLOYEE_ID,FIRST_NAME --value_cols LAST_NAME,EMAIL,PHONE_NUMBER,HIRE_DATE,JOB_ID,SALARY,COMMISSION_PCT,MANAGER_ID,DEPARTMENT_ID
-python automation.py --model_name piad_model_2 --key_cols EMPLOYEE_ID --value_cols LAST_NAME,EMAIL,PHONE_NUMBER
+python automation.py --model_name piad_model_1 --key_cols EMPLOYEE_ID,FIRST_NAME --value_cols LAST_NAME,EMAIL,PHONE_NUMBER,HIRE_DATE,JOB_ID,SALARY,COMMISSION_PCT,MANAGER_ID,DEPARTMENT_ID --relational_database_name https://gist.github.com/kevin336/acbb2271e66c10a5b73aacf82ca82784
+python automation.py --model_name piad_model_2 --key_cols EMPLOYEE_ID --value_cols LAST_NAME,EMAIL,PHONE_NUMBER --relational_database_name https://gist.github.com/kevin336/acbb2271e66c10a5b73aacf82ca82784 
